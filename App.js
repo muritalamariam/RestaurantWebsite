c8b70fb22d9615de6f7e07dd1d891749ae591759
muritalamariam <87655632+muritalamariam@users.noreply.github.com>
2022-04-26 23:58:45 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import { BrowserRouter as Router, Routes,Route,} from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Products from './components/Products/Products';
+import Contact from './components/Contact/Contact';
+
+function App() {
+  return (
+    
+   <Router>
+      <div className="App">
+        <Navbar />
+        <Contact />
+        <Products />
+      </div>
+     <Routes>
+       <Route path='/' element={<Home />} />
+       <Route path='/about' element={<About />} />
+       <Route path='/products' element={<Products />} />
+       <Route path='/contact' element={<Contact />} />
+     </Routes>
+
+   </Router>
+    
+  );
+}
+
+export default App;
